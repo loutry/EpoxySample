67c4023d93be298a04c1264af69f07358f219821
loutry <marion.hayoun@canal-plus.com>
2019-04-29 15:17:31 +0200

    domain - add showcase domain contract, clean model and use case

---
+++ app/src/main/java/fr/loutry/epoxysample/domain/model/showcase/ShowcasePage.kt
@@ -0,0 +1,18 @@
+package fr.loutry.epoxysample.domain.model.showcase
+
+data class ShowcasePage(val info: Info, val levels: List<Row>)
+
+data class Info(val title: String)
+
+data class Row(
+    val title: String,
+    val contents: List<Item>
+) {
+    data class Item(
+        val id: String,
+        val title: String,
+        val subtitle: String,
+        val imageUrl: String
+    )
+
+}
