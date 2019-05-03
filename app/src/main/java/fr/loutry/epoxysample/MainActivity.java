48a2de46ecc5669d8d1e51e7b951857e964b0d13
loutry <marion.hayoun@canal-plus.com>
2019-05-03 15:19:01 +0200

    ui - add showcase page fragment with its view model base

--- app/src/main/java/fr/loutry/epoxysample/MainActivity.java
+++ app/src/main/java/fr/loutry/epoxysample/MainActivity.java
@@ -1,8 +1,8 @@
 package fr.loutry.epoxysample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import fr.loutry.epoxysample.ui.showcase.ShowcaseFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, ShowcaseFragment.newInstance())
+                    .commitNow();
+        }
     }
 }
--- app/src/main/java/fr/loutry/epoxysample/di/KoinUtils.kt
+++ app/src/main/java/fr/loutry/epoxysample/di/KoinUtils.kt
@@ -2,6 +2,7 @@ package com.canal.android.mycanalpoc.di
 
 import fr.loutry.epoxysample.di.domainUseCaseModule
 import fr.loutry.epoxysample.di.pocEpoxyModule
+import fr.loutry.epoxysample.di.uiPageModule
 
 /**
  * KoinUtils -
@@ -12,6 +13,7 @@ object KoinUtils {
 
     val koinModules = listOf(
         pocEpoxyModule,
-        domainUseCaseModule
+        domainUseCaseModule,
+        uiPageModule
     )
 }
